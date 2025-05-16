a969c6c8537ced52015db381f6d7059863a2fb40
TonyXu1227 <txu122705@gmail.com>
2025-05-16 16:20:02 -0400

    Edited setup and tried installing setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@
 pip install -U discord
 pip install -U python-dotenv
 
-echo "Running instal colabfold"
-./install_colabbatch_M1mac.sh
-
+echo "Running install colabfold"
+bash install_colabbatch_M1mac.sh
 echo "Running inputbot"
 python inputbot.py
